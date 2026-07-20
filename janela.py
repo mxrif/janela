8e6485d85207c042cd084c22b2567f3baac67522
Mariana Ferraz <marianafc93@hotmail.com>
2026-07-20 07:42:27 -0300

    Initial commit

---
+++ janela.py
@@ -0,0 +1,10 @@
+from tkinter import Tk, Label, PhotoImage, TOP, BOTTOM
+
+root = Tk()
+photo = PhotoImage(file='photo.gif').subsample(1)
+image = Label(master=root, image=photo)
+image.pack(side=TOP)
+text = Label(master=root, font=("Courier", 18), text='Folipurba ist richtig cool')
+text.pack(side=BOTTOM)
+root.mainloop()
+
