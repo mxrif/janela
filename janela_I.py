1826c8e6e3b5373e5889b57859f3b0a42ea1c864
Mariana Ferraz <marianafc93@hotmail.com>
2026-07-21 10:05:29 -0300

    Criação de janela simples

--- janela_I.py
+++ janela_I.py
@@ -6,5 +6,6 @@ image = Label(master=root, image=photo)
 image.pack(side=TOP)
 text = Label(master=root, font=("Courier", 18), text='Folipurba ist richtig cool')
 text.pack(side=BOTTOM)
+# roda a janela
 root.mainloop()
 
